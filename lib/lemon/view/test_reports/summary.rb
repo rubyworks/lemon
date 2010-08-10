7f1f7b625110664747270b34ee9fc37192e921c3
7rans <transfire@gmail.com>
2010-08-10 07:20:31 -0400

    add summary test reporter

--- lib/lemon/view/test_reports/summary.rb
+++ lib/lemon/view/test_reports/summary.rb
@@ -2,8 +2,8 @@ require 'lemon/view/test_reports/abstract'
 
 module Lemon::TestReports
 
-  # Other Reporter
-  class Other < Abstract
+  # Summary Reporter
+  class Summary < Abstract
 
     #
     def start_suite(suite)
@@ -11,10 +11,10 @@ module Lemon::TestReports
     end
 
     #
-    def start_case(tc)
-      puts
-      puts tc.to_s.ansi(:bold)
-    end
+    #def start_case(tc)
+    #  puts
+    #  puts tc.to_s.ansi(:bold)
+    #end
 
     #
     #def report_instance(instance)
@@ -29,78 +29,87 @@ module Lemon::TestReports
     end
 
     #
-    def start_unit(unit)
-       instance = unit.instance
-       if @instance != instance
-         @instance = instance
-         puts
-         puts "  #{instance}"
-         puts
-       end
+    #def start_unit(unit)
+    #   context = unit.context
+    #   if @instance != context
+    #     @context = context
+    #     puts
+    #     puts "  #{context}"
+    #     puts
+    #   end
+    #end
+
+    #
+    def omit(unit)
+      puts "  %s  %s %s" % ["   OMIT".ansi(:cyan), unit.to_s.ansi(:bold), unit.aspect]
     end
 
     #
     def pass(unit)
-      puts "  %s  %s %s" % ["   PASS".ansi(:green), unit.name, unit.aspect]
+      puts "  %s  %s %s" % ["   PASS".ansi(:green), unit.to_s.ansi(:bold), unit.aspect]
     end
 
     #
     def fail(unit, exception)
-      puts "  %s  %s %s" % ["   FAIL".ansi(:red), unit.name, unit.aspect]
-      puts
-      puts "        FAIL #{exception.backtrace[0]}"
-      puts "        #{exception}"
-      puts
+      puts "  %s  %s %s" % ["   FAIL".ansi(:red), unit.to_s.ansi(:bold), unit.aspect]
+      #puts
+      #puts "        FAIL #{exception.backtrace[0]}"
+      #puts "        #{exception}"
+      #puts
     end
 
     #
     def error(unit, exception)
-      puts "  %s  %s %s" % ["  ERROR".ansi(:red), unit.name, unit.aspect]
-      puts
-      puts "        ERROR #{exception.class}"
-      puts "        #{exception}"
-      puts "        " + exception.backtrace.join("\n        ")
-      puts
+      puts "  %s  %s %s" % ["  ERROR".ansi(:red), unit.to_s.ansi(:bold), unit.aspect]
+      #puts
+      #puts "        ERROR #{exception.class}"
+      #puts "        #{exception}"
+      #puts "        " + exception.backtrace.join("\n        ")
+      #puts
     end
 
     #
     def pending(unit, exception)
-      puts "  %s  %s %s" % ["PENDING".ansi(:yellow), unit.name, unit.aspect]
+      puts "  %s  %s %s" % ["PENDING".ansi(:yellow), unit.to_s.ansi(:bold), unit.aspect]
     end
 
     #
     def finish_suite(suite)
-      #puts
-
-      #unless failures.empty?
-      #  puts "FAILURES:\n\n"
-      #  failures.each do |unit, exception|
-      #    puts "    #{unit}"
-      #    puts "    #{exception}"
-      #    puts "    #{exception.backtrace[0]}"
-      #    puts
-      #  end
-      #end
-
-      #unless errors.empty?
-      #  puts "ERRORS:\n\n"
-      #  errors.each do |unit, exception|
-      #    puts "    #{unit}"
-      #    puts "    #{exception}"
-      #    puts "    #{exception.backtrace[0]}"
-      #    puts
-      #  end
-      #end
-
-      #unless pendings.empty?
-      #  puts "PENDING:\n\n"
-      #  pendings.each do |unit, exception|
-      #    puts "    #{unit}"
-      #  end
-      #  puts
-      #end
-
       puts
+
+      unless record[:fail].empty?
+        puts "FAILURES:\n\n"
+        record[:fail].each do |unit, exception|
+          puts "    #{unit}"
+          puts "    #{file_and_line(exception)}"
+          puts "    #{exception}"
+          puts code_snippet(exception)
+          #puts "    #{exception.backtrace[0]}"
+          puts
+        end
+      end
+
+      unless record[:error].empty?
+        puts "ERRORS:\n\n"
+        record[:error].each do |unit, exception|
+          puts "    #{unit}"
+          puts "    #{file_and_line(exception)}"
+          puts "    #{exception}"
+          puts code_snippet(exception)
+          #puts "    #{exception.backtrace[0]}"
+          puts
+        end
+      end
+
+      unless record[:pending].empty?
+        puts "PENDING:\n\n"
+        record[:pending].each do |unit, exception|
+          puts "    #{unit}"
+          puts "    #{file_and_line(exception)}"
+          puts
+        end
+      end
+
       puts tally
     end
 
