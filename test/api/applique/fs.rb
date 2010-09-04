d92e62c811e10e8b64e2897b31fd87720bbc4ee0
7rans <transfire@gmail.com>
2010-09-04 07:07:15 -0400

    test: adjust tests to match code changes

--- test/api/applique/fs.rb
+++ test/api/applique/fs.rb
@@ -1,10 +1,16 @@
+require 'fileutils'
+
+Before :demo do
+  FileUtils.rm_r('tmp')
+end
+
 When "Given an example script in '(((.*?)))' as follows" do |name, text|
   name = File.join('tmp', name) if /^tmp/ !~ name
   FileUtils.mkdir_p(File.dirname(name))
   File.open(name, 'w'){ |w| w << text }
 end
 
-When "And given a test case in '(((.*?)))' as follows"  do |name, text|
+When "given a test case in '(((.*?)))' as follows"  do |name, text|
   name = File.join('tmp', name) if /^tmp/ !~ name
   FileUtils.mkdir_p(File.dirname(name))
   File.open(name, 'w'){ |w| w << text }
