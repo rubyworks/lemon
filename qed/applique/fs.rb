b901183c9d32f5f83778ba4e739fa711e85286a4
7rans <transfire@gmail.com>
2011-05-18 13:04:12 -0400

    test: qed demos get their own example files

--- qed/applique/fs.rb
+++ qed/applique/fs.rb
@@ -1,18 +1,21 @@
 require 'fileutils'
 
-Before :demo do
-  FileUtils.rm_r('tmp')
-end
+#Before :demo do
+#  FileUtils.rm_r('lib')
+#  FileUtils.rm_r('test')
+#end
 
 When "Given an example script in '(((.*?)))' as follows" do |name, text|
-  name = File.join('tmp', name) if /^tmp/ !~ name
-  FileUtils.mkdir_p(File.dirname(name))
+  #name = File.join('tmp', name) if /^tmp/ !~ name
+  dir = File.dirname(name)
+  FileUtils.mkdir_p(dir) unless File.directory?(dir)
   File.open(name, 'w'){ |w| w << text }
 end
 
 When "given a test case in '(((.*?)))' as follows"  do |name, text|
-  name = File.join('tmp', name) if /^tmp/ !~ name
-  FileUtils.mkdir_p(File.dirname(name))
+  #name = File.join('tmp', name) if /^tmp/ !~ name
+  dir = File.dirname(name)
+  FileUtils.mkdir_p(dir) unless File.directory?(dir)
   File.open(name, 'w'){ |w| w << text }
 end
 
