f84d08288b626cda39e4f2409d1356f2c1b2f80a
7rans <transfire@gmail.com>
2011-05-19 01:41:27 -0400

    test: a complete test should test the methods

--- test/fixtures/case_complete.rb
+++ test/fixtures/case_complete.rb
@@ -3,19 +3,23 @@ covers File.dirname(__FILE__) + '/example.rb'
 testcase X do
 
   unit :a => "Returns a String" do
+    X.new.a
   end
 
   unit :b => "Returns a String" do
+    X.new.b
   end
 
   unit :c => "Returns a String" do
+    X.new.c
   end
 
 end
 
-testrcase Y do
+testcase Y do
 
   unit :q => "Returns a String" do
+    Y.new.q
   end
 
 end
