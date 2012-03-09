89c7b40c1d0405c63da48fb8886f7d300dc8b23d
7rans <transfire@gmail.com>
2012-03-08 20:18:45 -0500

    Ignore callers is loaded by test_world.rb. [minor]

--- lib/lemon/test_world.rb
+++ lib/lemon/test_world.rb
@@ -1,3 +1,9 @@
+if RUBY_VERSION < '1.9'
+  require 'lemon/ignore_callers'
+else
+  require_relative 'ignore_callers'
+end
+
 module Lemon
 
   # The World module is the base class for all Lemon test scopes.
@@ -7,3 +13,4 @@ module Lemon
   end
 
 end
+
